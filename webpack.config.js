57ff3ee296ccc98111e839922671925b7f1afb79
Rene Garcia <rene.garciah90@gmail.com>
2016-11-27 07:37:20 -0600

    Adding components into index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,26 @@
-// In webpack.config.js
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/webapp/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
 module.exports = {
   entry: [
-    './app/index.js'
-  ]
+    './webapp/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: __dirname + 'webapp',
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'index_bundle.js',
+    path: __dirname + '/public'
+  },
+  plugins: [HTMLWebpackPluginConfig]
 }
